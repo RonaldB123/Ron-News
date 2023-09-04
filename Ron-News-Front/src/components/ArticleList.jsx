47414f176c2c6f6fc9e8ac8a93947dda9342c053
RonaldB123 <ronaldbeck@hotmail.co.uk>
2023-09-04 16:16:34 +0100

    added loading h1 tag

--- Ron-News-Front/src/components/ArticleList.jsx
+++ Ron-News-Front/src/components/ArticleList.jsx
@@ -5,12 +5,20 @@ import { useState } from "react"
 
 export const ArticleList = () => {
     const [articleData, setArticleData] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
 
     useEffect(() => {
         getArticles().then(({data}) => {
             setArticleData(data.articles);
+            setIsLoading(false);
         })
     }, [])
+
+    if(isLoading){
+        return (
+            <h1 className="loading-header">Loading...</h1>
+        )
+    }
 
     return (
     <div className="articleList-container">
